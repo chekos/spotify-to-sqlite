065a6a8f96da51396b678baeedb3dcf0b1732699
Sergio Sanchez <ssanchez@alluma.org>
2022-02-24 15:46:47 -0800

    add basic test

--- tests/test_spotify_to_sqlite.py
+++ tests/test_spotify_to_sqlite.py
@@ -1,5 +1,30 @@
-from spotify_to_sqlite import example_function
+import pytest
+from typer.testing import CliRunner
+
+from spotify_to_sqlite import __app_name__, __version__, cli
+
+runner = CliRunner()
 
 
-def test_example_function():
-    assert example_function() == 2
+@pytest.mark.parametrize(
+    "options",
+    (
+        ["-v"],
+        ["--version"],
+    ),
+)
+def test_version(options):
+    result = CliRunner().invoke(cli.cli, options)
+    assert result.exit_code == 0
+    assert f"{__app_name__} v{__version__}" in result.output
+
+
+@pytest.mark.parametrize(
+    "options",
+    (["--help"],),
+)
+def test_help(options):
+    result = CliRunner().invoke(cli.cli, options)
+    assert result.exit_code == 0
+    assert result.output.startswith("Usage: ")
+    assert "--help" in result.output
